06ff09ae5a5fd3ab0716b06de8a83783b17c0ac0
jaclynr109 <jaclynr6@mit.edu>
2026-02-21 23:51:32 -0500

    updated projects

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<nav>
+    <a href="./" class="current">Home</a> 
+    <a href="projects/">Projects</a> 
+    <a href="contact/">Contact</a> 
+    <a href="cv/">Resume</a> 
+    <a href="https://github.com/jaclynr109" target="_blank">GitHub</a>
+  </nav>
+  
+<h1>Jaclyn Rambarran</h1>
+<p>I am a graduate student at MIT in the Technology and Policy Program interested in decarbonized electric energy systems.</p>
+<img src="images/jaclyn-profile.jpeg" alt="Portrait of Jaclyn smiling indoors" width="300">
