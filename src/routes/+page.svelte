e29f5f1e423814b0195da9eb28f7ac1cfdcc14e6
jaclynr109 <jaclynr6@mit.edu>
2026-02-26 13:02:21 -0500

    port old site to svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <nav>
-    <a href="./" class="current">Home</a> 
-    <a href="projects/">Projects</a> 
-    <a href="contact/">Contact</a> 
-    <a href="cv/">Resume</a> 
-    <a href="https://github.com/jaclynr109" target="_blank">GitHub</a>
-  </nav>
-  
+  <a class="current" href=".">Home</a>
+  <a href="projects">Projects</a>
+  <a href="contact">Contact</a>
+  <a href="resume">Resume</a>
+  <a href="https://github.com/jaclynr109" target="_blank">GitHub</a>
+</nav>
+
 <h1>Jaclyn Rambarran</h1>
 <p>I am a graduate student at MIT in the Technology and Policy Program interested in decarbonized electric energy systems.</p>
 <img src="images/jaclyn-profile.jpeg" alt="Portrait of Jaclyn smiling indoors" width="300">
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,3 +1,29 @@
 <svelte:head>
-  <title>Contact me</title>
+  <title>Contact</title>
 </svelte:head>
+<nav>
+  <a href="../">Home</a> 
+  <a href="../projects/">Projects</a> 
+  <a href="../contact/" class="current">Contact</a> 
+  <a href="../cv/">Resume</a> 
+  <a href="https://github.com/jaclynr109" target="_blank">GitHub</a>
+</nav>
+<h1>Contact</h1>
+<form action="mailto:183286511+jaclynr109@users.noreply.github.com" method="GET" enctype="text/plain">
+  <label>
+      Email:
+      <input type="email" name="email" required>
+    </label>
+  
+    <label>
+      Subject:
+      <input type="text" name="subject" required>
+    </label>
+  
+    <label>
+      Message:
+      <textarea name="body" rows="5" cols="30" required></textarea>
+    </label>
+
+    <button>Send</button>
+  </form>
