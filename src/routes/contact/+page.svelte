0f0d85b0ca5c9801fd82495c1f64a386b25a45fc
jaclynr109 <jaclynr6@mit.edu>
2026-03-05 08:48:49 -0500

    update nav bar links

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -2,11 +2,13 @@
   <title>Contact</title>
 </svelte:head>
 <nav>
-  <a href="../">Home</a> 
-  <a href="../projects/">Projects</a> 
-  <a href="../contact/" class="current">Contact</a> 
-  <a href="../cv/">Resume</a> 
-  <a href="https://github.com/jaclynr109" target="_blank">GitHub</a>
+  <a href=".">Home</a>
+  <a href="projects">Projects</a>
+  <a class="current" href="contact">Contact</a>
+  <a href="resume">Resume</a>
+  <a href="https://github.com/jaclynr109" target="_blank" rel="noreferrer">
+    GitHub
+  </a>
 </nav>
 <h1>Contact</h1>
 <form action="mailto:183286511+jaclynr109@users.noreply.github.com" method="GET" enctype="text/plain">
