a23820e0887c7b93ae3176a0e701d0e53d05c143
jaclynr109 <jaclynr6@mit.edu>
2026-03-05 09:26:35 -0500

    append sveltekit's base

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
+<script>
+  import { base } from '$app/paths';
+</script>
+
 <nav>
-  <a class="current" href=".">Home</a>
-  <a href="projects">Projects</a>
-  <a href="contact">Contact</a>
-  <a href="resume">Resume</a>
-  <a href="https://github.com/jaclynr109" target="_blank" rel="noreferrer">
-    GitHub
-  </a>
+  <a href={`${base}/`}>Home</a>
+  <a href={`${base}/projects`}>Projects</a>
+  <a href={`${base}/contact`}>Contact</a>
+  <a href={`${base}/resume`}>Resume</a>
+  <a href="https://github.com/jaclynr109" target="_blank" rel="noreferrer">GitHub</a>
 </nav>
 
 <slot />
