11f5b6374aa548cac2141ccac705f498f057e621
jaclynr109 <jaclynr6@mit.edu>
2026-03-05 13:52:59 -0500

    add light to dark compatibility

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,8 @@
   import { page } from "$app/stores";
   import { base } from "$app/paths";
 
+  let localStorage = globalThis.localStorage ?? {};
+
   let pages = [
     { url: "/", title: "About" },
     { url: "/projects", title: "Projects" },
@@ -9,7 +11,28 @@
     { url: "/contact", title: "Contact" },
     { url: "https://github.com/jaclynr109", title: "Github" }
   ];
+
+  let colorScheme = "light dark";
+
+  if (localStorage.colorScheme) {
+  colorScheme = localStorage.colorScheme;
+  }
+  
+  let root = globalThis.document?.documentElement;
+
+  $: root?.style.setProperty("color-scheme", colorScheme);
+
+  $: localStorage.colorScheme = colorScheme;
 </script>
+
+<label class="color-scheme-switch">
+  Theme:
+  <select bind:value={colorScheme}>
+    <option value="light dark">Automatic</option>
+    <option value="light">Light</option>
+    <option value="dark">Dark</option>
+  </select>
+</label>
 
 <nav>
   {#each pages as p}
@@ -27,3 +50,52 @@
 </nav>
 
 <slot />
+
+<style>
+  :root {
+    /* Use "dark" temporarily to test, then change to "light dark" */
+    color-scheme: light dark;
+  }
+
+  body {
+    background: canvas;
+    color: canvastext;
+  }
+
+  nav {
+    --border-color: oklch(50% 10% 200 / 40%);
+
+    display: flex;
+    margin-bottom: 1.5em;
+    border-bottom: 1px solid var(--border-color);
+  }
+
+  nav a {
+    flex: 1;
+    text-align: center;
+    padding: 0.5em;
+    text-decoration: none;
+    color: inherit;
+  }
+
+  nav a.current {
+    border-bottom: 0.4em solid var(--border-color);
+    padding-bottom: 0.1em;
+  }
+
+  nav a:hover {
+    border-bottom: 0.4em solid var(--color-accent);
+    background-color: color-mix(in oklch, var(--color-accent), canvas 85%);
+  }
+  .color-scheme-switch {
+  position: absolute;
+  top: 1rem;
+  right: 1rem;
+
+  display: inline-flex;
+  align-items: center;
+  gap: 4px;
+
+  font-size: 80%;
+}
+</style>
