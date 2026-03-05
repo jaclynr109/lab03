5798f526dd73b6d0a750adf3205c9130c594ea0e
jaclynr109 <jaclynr6@mit.edu>
2026-03-05 09:12:23 -0500

    deleted commented out nav bars

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,16 +6,6 @@
   import ReadingItem from "$lib/ReadingItem.svelte";
 </script>
 
-<!-- <nav>
-  <a class="current" href=".">Home</a>
-  <a href="projects">Projects</a>
-  <a href="contact">Contact</a>
-  <a href="resume">Resume</a>
-  <a href="https://github.com/jaclynr109" target="_blank" rel="noreferrer">
-    GitHub
-  </a>
-</nav> -->
-
 <h1>Jaclyn Rambarran</h1>
 
 <p>
