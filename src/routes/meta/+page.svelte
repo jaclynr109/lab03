3b07ffb979678d064561b3743b3c6eb768db47f0
jaclynr109 <jaclynr6@mit.edu>
2026-04-02 11:27:30 -0400

    add basic tool tip

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -23,6 +23,9 @@
   let yAxis;
   let yAxisGridlines;
 
+  let hoveredIndex = -1;
+  $: hoveredCommit = commits[hoveredIndex] ?? hoveredCommit ?? {};
+
   $: colorScale = d3.scaleLinear()
     .domain([0, 12, 24])
     .range(["#1f3b73", "#f4a261", "#1f3b73"]);
@@ -54,6 +57,8 @@
         lines
       };
     });
+
+    commits = d3.sort(commits, d => -d.totalLines);
   });
 
   $: [minDate, maxDate] = d3.extent(commits.map(d => d.date));
@@ -74,7 +79,7 @@
 
   $: [minLines, maxLines] = d3.extent(commits.map(d => d.totalLines));
 
-  $: rScale = d3.scaleLinear()
+  $: rScale = d3.scaleSqrt()
     .domain(minLines && maxLines ? [minLines, maxLines] : [1, 1])
     .range([5, 30]);
 
@@ -104,39 +109,85 @@
 
 <h3>Commits by time of day</h3>
 
-<svg viewBox="0 0 {width} {height}">
-  <g
-    class="gridlines"
-    transform="translate({usableArea.left}, 0)"
-    bind:this={yAxisGridlines}
-  />
+<div class="chart-layout">
+  <dl class="info">
+    <dt>Commit</dt>
+    <dd><a href={hoveredCommit.url} target="_blank">{hoveredCommit.id}</a></dd>
 
-  <g
-    transform="translate({usableArea.left}, 0)"
-    bind:this={yAxis}
-  />
+    <dt>Author</dt>
+    <dd>{hoveredCommit.author}</dd>
 
-  <g
-    transform="translate(0, {usableArea.bottom})"
-    bind:this={xAxis}
-  />
+    <dt>Date</dt>
+    <dd>{hoveredCommit.datetime?.toLocaleString("en", { dateStyle: "full" })}</dd>
 
-  <g class="dots">
-    {#each commits as commit, index}
-      <circle
-        cx={xScale(commit.datetime)}
-        cy={yScale(commit.hourFrac)}
-        r={rScale(commit.totalLines)}
-        fill="steelblue"
-        fill-opacity="0.7"
-      />
-    {/each}
-  </g>
-</svg>
+    <dt>Time</dt>
+    <dd>{hoveredCommit.datetime?.toLocaleString("en", { timeStyle: "short" })}</dd>
+
+    <dt>Lines</dt>
+    <dd>{hoveredCommit.totalLines}</dd>
+  </dl>
+
+  <svg viewBox="0 0 {width} {height}">
+    <g
+      class="gridlines"
+      transform="translate({usableArea.left}, 0)"
+      bind:this={yAxisGridlines}
+    />
+
+    <g
+      transform="translate({usableArea.left}, 0)"
+      bind:this={yAxis}
+    />
+
+    <g
+      transform="translate(0, {usableArea.bottom})"
+      bind:this={xAxis}
+    />
+
+    <g class="dots">
+      {#each commits as commit, index}
+        <circle
+          cx={xScale(commit.datetime)}
+          cy={yScale(commit.hourFrac)}
+          r={rScale(commit.totalLines)}
+          fill="steelblue"
+          fill-opacity="0.7"
+          on:mouseenter={() => hoveredIndex = index}
+          on:mouseleave={() => hoveredIndex = -1}
+        />
+      {/each}
+    </g>
+  </svg>
+</div>
 
 <style>
+  .chart-layout {
+    display: grid;
+    grid-template-columns: 220px 1fr;
+    gap: 1.5rem;
+    align-items: start;
+  }
+
   svg {
     overflow: visible;
+  }
+
+  dl.info {
+    display: grid;
+    grid-template-columns: max-content 1fr;
+    gap: 0.35em 1em;
+    margin: 0;
+    align-content: start;
+  }
+
+  dl.info dt {
+    margin: 0;
+    color: #777;
+    font-weight: 400;
+  }
+
+  dl.info dd {
+    margin: 0;
   }
 
   .gridlines {
@@ -145,5 +196,18 @@
 
   .gridlines line {
     stroke-width: 1.5;
+  }
+
+  circle {
+    transition: 200ms;
+  }
+
+  .dots:hover circle {
+    opacity: 0.3;
+  }
+
+  .dots circle:hover {
+    opacity: 1;
+    fill: darkgreen;
   }
 </style>
