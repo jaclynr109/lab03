e404cca090797489b7649624d849057b973ebaff
jaclynr109 <jaclynr6@mit.edu>
2026-04-02 11:09:11 -0400

    change commit marker by number of lines changed

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -2,11 +2,30 @@
   import { base } from '$app/paths';
   import { onMount } from 'svelte';
   import * as d3 from 'd3';
-  import BarHorizontal from '$lib/BarHorizontal.svelte';
+
+  let width = 1000, height = 600;
+
+  let margin = { top: 20, right: 20, bottom: 30, left: 50 };
+
+  let usableArea = {
+    top: margin.top,
+    right: width - margin.right,
+    bottom: height - margin.bottom,
+    left: margin.left
+  };
+  usableArea.width = usableArea.right - usableArea.left;
+  usableArea.height = usableArea.bottom - usableArea.top;
 
   let locData = [];
-  let barData = [];
   let commits = [];
+
+  let xAxis;
+  let yAxis;
+  let yAxisGridlines;
+
+  $: colorScale = d3.scaleLinear()
+    .domain([0, 12, 24])
+    .range(["#1f3b73", "#f4a261", "#1f3b73"]);
 
   onMount(async () => {
     locData = await d3.csv(`${base}/loc.csv`, row => ({
@@ -22,9 +41,9 @@
       let first = lines[0];
       let { author, date, time, timezone, datetime } = first;
 
-      let ret = {
+      return {
         id: commit,
-        url: "https://github.com/jaclynr109/lab03/commit/" + commit,
+        url: "https://github.com/jaclynr109/lb03/commit/" + commit,
         author,
         date,
         time,
@@ -32,23 +51,99 @@
         datetime,
         hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
         totalLines: lines.length,
-        lines: lines
+        lines
       };
-
-      return ret;
     });
-
-    // console.log(commits);
   });
+
+  $: [minDate, maxDate] = d3.extent(commits.map(d => d.date));
+
+  $: maxDatePlusOne = maxDate ? new Date(maxDate) : null;
+  $: if (maxDatePlusOne) {
+    maxDatePlusOne.setDate(maxDatePlusOne.getDate() + 1);
+  }
+
+  $: xScale = d3.scaleTime()
+    .domain(minDate && maxDatePlusOne ? [minDate, maxDatePlusOne] : [new Date(), new Date()])
+    .range([usableArea.left, usableArea.right])
+    .nice();
+
+  $: yScale = d3.scaleLinear()
+    .domain([24, 0])
+    .range([usableArea.bottom, usableArea.top]);
+
+  $: [minLines, maxLines] = d3.extent(commits.map(d => d.totalLines));
+
+  $: rScale = d3.scaleLinear()
+    .domain(minLines && maxLines ? [minLines, maxLines] : [1, 1])
+    .range([5, 30]);
+
+  $: if (xAxis && xScale) {
+    d3.select(xAxis).call(d3.axisBottom(xScale));
+  }
+
+  $: if (yAxis && yScale) {
+    d3.select(yAxis).call(
+      d3.axisLeft(yScale)
+        .tickFormat(d => String(d % 24).padStart(2, '0') + ':00')
+    );
+  }
+
+  $: if (yAxisGridlines && yScale) {
+    const axis = d3.axisLeft(yScale)
+      .tickFormat('')
+      .tickSize(-usableArea.width);
+
+    d3.select(yAxisGridlines).call(axis);
+
+    d3.select(yAxisGridlines)
+      .selectAll('.tick line')
+      .attr('stroke', d => colorScale(d));
+  }
 </script>
 
-<svelte:head>
-  <title>Meta</title>
-</svelte:head>
+<h3>Commits by time of day</h3>
 
-<h1>Meta</h1>
-<p>Meta page to visualize project data.</p>
+<svg viewBox="0 0 {width} {height}">
+  <g
+    class="gridlines"
+    transform="translate({usableArea.left}, 0)"
+    bind:this={yAxisGridlines}
+  />
 
-{#if barData.length > 0}
-  <BarHorizontal data={barData} />
-{/if}
+  <g
+    transform="translate({usableArea.left}, 0)"
+    bind:this={yAxis}
+  />
+
+  <g
+    transform="translate(0, {usableArea.bottom})"
+    bind:this={xAxis}
+  />
+
+  <g class="dots">
+    {#each commits as commit, index}
+      <circle
+        cx={xScale(commit.datetime)}
+        cy={yScale(commit.hourFrac)}
+        r={rScale(commit.totalLines)}
+        fill="steelblue"
+        fill-opacity="0.7"
+      />
+    {/each}
+  </g>
+</svg>
+
+<style>
+  svg {
+    overflow: visible;
+  }
+
+  .gridlines {
+    stroke-opacity: 0.3;
+  }
+
+  .gridlines line {
+    stroke-width: 1.5;
+  }
+</style>
